a7e1241f9e2844aa7fd1690c70522335612d479e
Nastya Tsimanovich <nastya.cimanovich.97@gmail.com>
2019-10-01 11:06:39 +0300

    feat: add solution morse-decode

--- src/index.js
+++ src/index.js
@@ -39,6 +39,53 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let array_code;
+    let array_code_10=[];
+    let letter_morse='';
+    let array_morse=[];
+    let result=[];
+    let letter='';
+
+    array_code=expr.split("");
+    for (let i = 0; i < array_code.length; i+=10) {
+        array_code_10.push(array_code.slice(i, i + 10));
+    }
+    // console.log(array_code_10);
+    
+    for(let i=0; i<array_code_10.length; i++){
+       for(let j=0; j<10; j++){
+        //    console.log(array_code_10[i][j]);
+            if(array_code_10[i][j]==1 && array_code_10[i][j+1]==0){
+                letter_morse+='.';
+                j++;
+            }
+            if(array_code_10[i][j]==1 && array_code_10[i][j+1]==1){
+                letter_morse+='-';
+                j++;
+            }
+            if(array_code_10[i][j]=="*"){
+                letter_morse+='';
+                j+=10;
+            }
+           
+       }    
+       array_morse.push(letter_morse);
+       letter_morse='';
+    //    console.log(array_morse);
+
+    }
+    for (let y=0; y<array_morse.length; y++){
+        if(array_morse[y]!=''){
+         letter=MORSE_TABLE[array_morse[y]];
+         result.push(letter);
+        }
+        else{
+            result.push(' ');
+        }
+    }
+    result.join(',');
+    // console.log(result.join(''));
+    return result.join('');
 }
 
 module.exports = {
